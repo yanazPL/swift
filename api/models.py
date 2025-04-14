c90f428e0b96090655979184bca05fc55708fa64
Jan Gregorczyk <jnk.gregorczyk@gmail.com>
2025-04-14 21:36:32 +0200

    add Code model

--- api/models.py
+++ api/models.py
@@ -1,19 +1,15 @@
 from django.db import models
 
 class Code(models.Model):
-    """
-    "address": string,
-    "bankName": string,
-    "countryISO2": string,
-    "countryName": string,
-    “isHeadquarter”: bool,
-    "swiftCode": string,
-
-    """
     address = models.CharField(blank=True)
     bank_name = models.CharField()
     country_iso_2 = models.CharField()
-    country_name = models.CharField
-    swift_code = models.CharField()
+    country_name = models.CharField()
+    swift_code = models.CharField(unique=True)
     is_headquarter = models.BooleanField()
-    headquarter = models.ForeignKey("self", on_delete=models.CASCADE, related_name="branch", null=True)
+    headquarter = models.ForeignKey("self", on_delete=models.CASCADE, related_name="branches", null=True)
+
+    def save(self, *args, **kwargs):
+        self.country_name = self.country_name.upper()
+        self.country_iso_2 = self.country_iso_2.upper()
+        super().save(*args, **kwargs)
