31e5489b25181cd596706db191e51c3b85af94fb
Jan Gregorczyk <jnk.gregorczyk@gmail.com>
2025-04-20 16:31:39 +0200

    restructure repo and properly contenerize

--- app/SWIFT/settings.py
+++ app/SWIFT/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'SWIFT.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.getenv('DJANGO_DB_PATH', BASE_DIR / 'db.sqlite3'),
     }
 }
 
